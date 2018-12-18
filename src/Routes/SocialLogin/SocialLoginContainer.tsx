df6492b512cd0d7bd8ff9ccf633e421cd65aac5f
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-12-18 14:54:25 +0900

    FaceLogin Done

--- src/Routes/SocialLogin/SocialLoginContainer.tsx
+++ src/Routes/SocialLogin/SocialLoginContainer.tsx
@@ -1,10 +1,11 @@
 import React, {Component} from "react";
-import { Mutation } from 'react-apollo';
+import { graphql,Mutation } from "react-apollo";
 import { toast } from 'react-toastify';
+import { LOG_USER_IN } from 'src/sharedQueries';
 import SocialLoginPresenter from "./SocialLoginPresenter";
 import { FACEBOOK_CONNECT } from './SocialLoginQueries';
 
-class SocialLoginContainer extends Component {
+class SocialLoginContainer extends Component<any> {
     public state = {
         email:"",
         fbId:"",
@@ -18,7 +19,26 @@ class SocialLoginContainer extends Component {
         return <Mutation mutation={FACEBOOK_CONNECT} variables={{email, 
         fbId, 
         firstName, 
-        lastName}} >
+        lastName}}
+        onCompleted={(data)=>{
+            console.log(data);
+            const { FacebookConnect: {token}} = data;
+            if(token ){
+                
+                toast.success("Logging you in...");
+                setTimeout(() => {
+                    this.props.logUserIn({
+                      variables: { token }
+                    });
+                }, 2500);
+            }else {
+                toast.error("Fail to find user Token", {
+                    position:toast.POSITION.BOTTOM_CENTER
+                })
+            }
+
+        }}
+        >
         {(mutation, {data}) => {
             this.mutation = mutation;
             return <SocialLoginPresenter loginCallBack={this.loginCallback} />;
@@ -28,7 +48,7 @@ class SocialLoginContainer extends Component {
     }
 
     public loginCallback = (response) => {
-        console.log(response)
+        
         const {first_name, last_name, email, id, name, accessToken} = response;
         if(accessToken){
             toast.success(`Welcome ${name}`, {
@@ -49,4 +69,6 @@ class SocialLoginContainer extends Component {
     }
 }
 
-export default SocialLoginContainer;
+export default graphql(LOG_USER_IN, {
+    name:"logUserIn"
+})(SocialLoginContainer);
